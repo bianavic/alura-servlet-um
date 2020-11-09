b0733ad13a9410822966154de92e2578e3294b5f
bianavic <bianavic@gmail.com>
2020-11-09 15:05:05 -0300

    tomcat, singleton, ioc

--- src/main/java/alura/javaservletum/HelloServletWorld.java
+++ src/main/java/alura/javaservletum/HelloServletWorld.java
@@ -14,6 +14,16 @@ import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/hello") // anotacao para mapear a URL para uma servlet
 public class HelloServletWorld extends HttpServlet {
 
+    // TomCat == middleware / intermediario == ele é um container
+
+    // Singleton == instancia unica dentro do servlet /
+    // singleton = uma vez criado (enqto estiver no ar) ele vive para sempre
+    // Mas só será criado depois de chamá-lo, porque qdo roda apenas deixa em standby
+
+    // inversao de controle == IOC / ao inves do meu metodo main instanciar o objeto,
+    // quem cuida do objeto é o Tomcat
+
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         // definir a resposta
